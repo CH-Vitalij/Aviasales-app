0b78a3f0f5d1577320a26c71325ee47d9b8d44a4
CH-Vitalij <v.charkin2017@gmail.com>
2024-08-15 18:12:14 +0500

    delete ReduxDevTools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import { rootReducer } from "./reducers";
-import { composeWithDevTools } from "@redux-devtools/extension";
 import { thunk } from "redux-thunk";
 
-export const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 export type AppDispatch = typeof store.dispatch;
